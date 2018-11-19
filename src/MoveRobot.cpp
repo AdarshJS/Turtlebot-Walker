d4ba30b2905544cfda013ca73d0cc5cec4be8b5f
Adarsh Jagan S <adarshjagan3895@gmail.com>
2018-11-18 20:54:27 -0500

    src: Added Doxygen comments to cpp files.

--- src/MoveRobot.cpp
+++ src/MoveRobot.cpp
@@ -1,8 +1,41 @@
-/*
- * MoveRobot.cpp
+/**
+ * Copyright (c) 2018, Adarsh Jagan Sathyamoorthy
  *
- *  Created on: Nov 18, 2018
- *      Author: root
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file       MoveRobot.cpp
+ * @author     Adarsh Jagan Sathyamoorthy
+ * @copyright  3-clause BSD
+ * @brief MoveRobot class' function definitions
+ * Defines the methods for the MoveRobot class.
  */
 
 #include "MoveRobot.hpp"
@@ -33,9 +66,11 @@ MoveRobot::~MoveRobot() {
 
 void MoveRobot::publishVelocity() {
   if (scan.returnCollisionFlag() == true) {
+    // Rotate CCW if collision is detected.
     vel.linear.x = 0.0;
     vel.angular.z = 1.0;
   } else {
+    // Move forward if no collision is detected
     vel.linear.x = 0.5;
     vel.angular.z = 0.0;
   }
